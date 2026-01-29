ee82097aa6f8af38b13736818d63136bb035e7fc
AidanJHernandez <aidanh37@nycstudents.net>
2026-01-29 10:10:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 //const radioInput = document.querySelector("#answer");
 const radioOutput1 = document.querySelector("#radio-output1");
 const radioOutput2 = document.querySelector("#radio-output2");
-
-
-
-
 const radioInput = document.querySelector("#answer");
 
 const displayAnswer = (e) => {
@@ -15,7 +11,12 @@ const displayAnswer = (e) => {
     radioOutput2.innerHTML = 
     `<p>Neat, right? How did the "Choose One." text change?</p>
     <p>Well, lemme show you. :D</p>
-    <p><img src="radioSS1.png" alt="Code" width="450" height="80"></p>`
+    <h6 style="transform: translate(0%, 100%)">(For sake of time, only example for choice one will be shown.)</h6>
+    <p><img src="radioSS1.png" alt="Code" width="450" height="80"></p>
+    <p style="transform: translate(0%, -6%)"><img src="radioSS2.png" alt="Code" width="450" height="245"></p>`
+
+radioInput.addEventListener('change', displayAnswer);
+
 
     } else if (Answer == "2"){
     radioOutput1.innerHTML = `<p>You picked checkbox 2.</p>`
