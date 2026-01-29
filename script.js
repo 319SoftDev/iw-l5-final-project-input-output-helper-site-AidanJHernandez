c473ae82f4353dc1857d83c777d9009c5fd10cbe
AidanJHernandez <aidanh37@nycstudents.net>
2026-01-29 10:00:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const radioInput = document.querySelector("#answer");
+//const radioInput = document.querySelector("#answer");
 const radioOutput1 = document.querySelector("#radio-output1");
 const radioOutput2 = document.querySelector("#radio-output2");
+
+
+
+
+const radioInput = document.querySelector("#answer");
 
 const displayAnswer = (e) => {
     const Answer = e.target.value;
@@ -10,7 +15,7 @@ const displayAnswer = (e) => {
     radioOutput2.innerHTML = 
     `<p>Neat, right? How did the "Choose One." text change?</p>
     <p>Well, lemme show you. :D</p>
-    <p></p>`
+    <p><img src="radioSS1.png" alt="Code" width="450" height="80"></p>`
 
     } else if (Answer == "2"){
     radioOutput1.innerHTML = `<p>You picked checkbox 2.</p>`
