b08defb5c93aad2a37847fca66d7017f10e8a2dd
AidanJHernandez <aidanh37@nycstudents.net>
2026-02-03 10:40:15 -0500

    a

--- script.js
+++ script.js
@@ -9,37 +9,37 @@ const displayAnswer = (e) => {
      if (Answer == "1"){
     radioOutput1.innerHTML = `<p>🍎✓</p>`
     radioOutput2.innerHTML = 
-    `<p>Answer locked in? Click the link below to find out what type of person you are! :D</p>`
+    `<p>Answer locked in? Click the link to move on to the next question! :D</p>`
     radioOutput3.innerHTML = 
-    `<a href="https://thegeekboiz.github.io/PythonScratcher/youareanidiot/index.html"><p>Click Me!!</p></a>`
+    `<p>Click Me!!</p>`
 
     } else if (Answer == "2"){
     radioOutput1.innerHTML = `<p>🥦✓</p>`
     radioOutput2.innerHTML = 
     `<p>Answer locked in? Click the link below to find out what type of person you are! :D</p>`
     radioOutput3.innerHTML = 
-    `<a href="https://thegeekboiz.github.io/PythonScratcher/youareanidiot/index.html"><p>Click Me!!</p></a>`
+    `<p>Click Me!!</p>`
 
     } else if (Answer == "3"){
     radioOutput1.innerHTML = `<p>🥩✓</p>`
     radioOutput2.innerHTML = 
     `<p>Answer locked in? Click the link below to find out what type of person you are! :D</p>`
     radioOutput3.innerHTML = 
-    `<a href="https://thegeekboiz.github.io/PythonScratcher/youareanidiot/index.html"><p>Click Me!!</p></a>`
+    `<p>Click Me!!</p>`
 
     } else if (Answer == "4"){
     radioOutput1.innerHTML = `<p>🍞✓</p>`
     radioOutput2.innerHTML = 
     `<p>Side question. How much bread do you eat in a day?</p>`
     radioOutput3.innerHTML = 
-    `<a href="https://thegeekboiz.github.io/PythonScratcher/youareanidiot/index.html"><p>Click Me!!</p></a>`
+    `<p>Click Me!!</p>`
     
     } else if (Answer == "5"){
     radioOutput1.innerHTML = `<p>🐄✓</p>`
     radioOutput2.innerHTML = 
     `<p>Answer locked in? Click the link below to find out what type of person you are! :D</p>`
     radioOutput3.innerHTML = 
-    `<a href="https://thegeekboiz.github.io/PythonScratcher/youareanidiot/index.html"><p>Click Me!!</p></a>`
+    `<p>Click Me!!</p>`
     }
 
 };
