5bc76fc57c5dc0af8fc30837e32be09fbd8bae4e
AidanJHernandez <aidanh37@nycstudents.net>
2026-02-05 09:58:43 -0500

    df

--- script4.js
+++ script4.js
@@ -1,22 +1,27 @@
-const textInput = document.querySelector("#submit");
+const textInput = document.querySelector("#submitButton");
 const textOutput1 = document.querySelector("#text-output1");
 const textOutput2 = document.querySelector("#text-output2");
 const textOutput3 = document.querySelector("#text-output3");
 
+console.log(textInput, textOutput1, textOutput2, textOutput3)
+
+
+
 const displaySubmit = (e) => {
     const Submit = e.target.value;
+    console.log(Submit);
 
      if (Submit == ""){
     textOutput1.innerHTML = `<p>✖</p>`
     textOutput2.innerHTML = 
-    `<p>No, seriously. I need a name.</p>`
+    `<p>Okay... nice try trickster.</p>`
     textOutput3.innerHTML = 
     `<p></p>`
     } 
     else {
     textOutput1.innerHTML = `<p>✓</p>`
     textOutput2.innerHTML = 
-    `<p>Thank you. :3</p>`
+    `<p>Thank you, ${Submit}. :3</p>`
     textOutput3.innerHTML = 
     `<p>Next Question</p>`
 
@@ -24,4 +29,4 @@ const displaySubmit = (e) => {
 };
 
 
-textInput.addEventListener('change', displaySubmit);
+fname.addEventListener('change', displaySubmit);
